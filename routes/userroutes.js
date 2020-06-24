91a82c0575f3f25d1262e6fb8664f734958b54aa
B_Shashidhar <shashi.2346@gmail.com>
2020-06-24 18:54:21 +0530

    user router
    user router updated

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,6 +1,6 @@
 var Users = require('../controllers/userscontroller');
 module.exports = function(router) {
-    router.post('/create', Users.createUser);
+    router.post('/user/create', Users.createUser);
     router.get('/getusers', Users.getUsers);
     router.get('/getuser/:username', Users.getUser);
     router.put('/updateuser/:email', Users.updateUser);
